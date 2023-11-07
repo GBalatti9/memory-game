a791b9ef02dd53ff3fc9300df1272f4fda859852
GBalatti9 <gas.balatti@gmail.com>
2023-11-07 12:29:36 -0300

    Styles need to be added

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,13 @@ export const App = () => {
   const { findGif, updateSetClickImages, resetClickImages } = checkUserResponse();
   const [bestScore, setBestScore] = useState(0);
 
-  const handleNotFoundImageId = ( index ) => {
+  const handleNotFoundImageId = (index) => {
     updateClickCounter();
     updateSetClickImages(index);
   }
 
   const handleFoundImageId = () => {
-    if(clickCounter > bestScore){
+    if (clickCounter > bestScore) {
       setBestScore(clickCounter)
     }
     resetCounter();
@@ -45,6 +45,10 @@ export const App = () => {
       {
         isLoading && <h3>Loading...</h3>
       }
+      <div>
+        <h2>Michael Scott Memory Game</h2>
+        <p>Get points by clicking on an image but don't click on any more than once!</p>
+      </div>
       <Counter clickCounter={clickCounter} bestScore={bestScore} />
       <div className="grid">
         {
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 export const Counter = ({ clickCounter, bestScore }) => {
 
